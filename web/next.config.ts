9f06bcb5a97ed78bab3739c9c0df07f7552ed4b4
toryoto <ryoto.taga12@gmail.com>
2025-12-29 01:07:05 +0900

    CI環境でBuildが落ちるバグの修正

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '..'),
-  },
 };
 
 export default nextConfig;
